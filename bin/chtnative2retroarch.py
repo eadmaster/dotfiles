33e8bb7dd4c33b5cfc5cbfeb4c3d7708ceef5d5f
a.mantoni <925171+eadmaster@users.noreply.github.com>
2023-05-09 01:08:39 +0200

    scripted commit

---
+++ bin/chtnative2retroarch.py
@@ -0,0 +1,84 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+
+if __name__ == "__main__":
+	import sys
+	import json
+	
+	
+	input_cht_file_str = open(sys.argv[1], "r").read()
+	
+	input_sys_str = None
+	if len(sys.argv) > 2:
+		# optional: specify the system
+		input_sys_str = sys.argv[2]
+	
+	
+	cheat_counter = 0
+	
+	for line in input_cht_file_str.splitlines():
+		if line.strip() == "" or line.startswith("#"):
+			# keep empty lines and comments
+			print(line)
+			continue
+
+		if "=" in line:
+			line_desc, line_value = line.split("=")
+			line_desc = line_desc.strip()
+			line_value = line_value.strip()
+			line_value = line_value.strip("\"")
+			
+		if line.startswith("cheats ") or line_desc.endswith("_enable"):
+			# skip cheat counter
+			continue
+			
+		if line_desc.endswith("_desc"):
+			print(line) # keep unchanged
+			code_description = line_value.strip("\"")
+		
+		if line_desc.endswith("_code"):
+			# perform conversion
+			for i, code in enumerate(line_value.split("+")):
+				address, value = code.split(" ")
+				#if not address.startswith("0"):
+				#	print("warn: address does not start with 0-, may needs decryption? (code skipped): " + address)
+				#	continue
+				
+				address = address[2:]  # cut 1st 2 digits , used to identify GS code type 
+				
+				if input_sys_str in ["sat", "ss", "saturn"]:
+					# need to apply a custom offset, tested with libretro-yabause only
+					address = address[1:]
+					address = "1" + address
+		
+				# TODO: properly parse code type, depends on system https://macrox.gshi.org/The%20Hacking%20Text.htm#playstation_code_types
+				
+				if i>0:
+					# repeat the description
+					print("")
+					print("cheat%d_desc = \"%s (part %d)\"" % (cheat_counter, code_description, i+1))
+				
+				print("cheat%d_address = \"%d\"" % (cheat_counter, int(address, 16)))
+				print("cheat%d_value = \"%d\"" % (cheat_counter, int(value, 16)))
+				print("cheat%d_cheat_type = \"1\"" % (cheat_counter))
+				print("cheat%d_handler = \"1\"" % (cheat_counter))
+				print("cheat%d_enable = false" % (cheat_counter))
+			
+				#if strlen(value)==2:
+				#	# assume 8-bit
+				if len(value)==4:
+					# assume 16-bit
+					print("cheat%d_memory_search_size = \"4\"" % (cheat_counter))
+				else:
+					# assume 32-bit
+					print("cheat%d_memory_search_size = \"5\"" % (cheat_counter))
+				cheat_counter += 1
+		# end if new code
+	# end for lines
+	
+	# print the correct cheats counter
+	print("cheats = \"%d\"" % (cheat_counter))
+	
+
+		
